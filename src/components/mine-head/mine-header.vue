94c8141e508e07e5c05763fd526c3759739c1765
baipeng <baipeng@linkv.sg>
2024-02-23 17:04:38 +0800

    init

---
+++ src/components/mine-head/mine-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <view style="background-color: darkgrey;height: 120px;align-items: center;">
+    <text>头部</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "mine-header"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/mine/mine-personal.vue
@@ -0,0 +1,37 @@
+<template>
+  <view style="background-color: #c8c7cc;">
+    <editor id="editor" class="ql-container" :placeholder="placeholder" @ready="onEditorReady"></editor>
+
+  </view>
+</template>
+
+<script>
+export default {
+  name: "mine-personal",
+  data() {
+    return {
+      placeholder: '开始输入...'
+    }
+  },
+  methods: {
+    onEditorReady() {
+      // #ifdef MP-BAIDU
+      this.editorCtx = requireDynamicLib('editorLib').createEditorContext('editor');
+      // #endif
+
+      // #ifdef APP-PLUS || H5 ||MP-WEIXIN
+      uni.createSelectorQuery().select('#editor').context((res) => {
+        this.editorCtx = res.context
+      }).exec()
+      // #endif
+    },
+    undo() {
+      this.editorCtx.undo()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
